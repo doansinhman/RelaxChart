b12aa4d79b6baedf5d755ba81abcae3a3fabe24e
Doan Sinh Man <man.doansinh@shopee.com>
2023-01-10 22:37:57 +0700

    original data

--- data.js
+++ data.js
@@ -1,25 +1,11 @@
 const DATA = 
-`Product_Owner,Team,Stakeholder,Predictable_Velocity,Time to_Market,Value_Delivered,Quality,Response_to Change,Product_Owner,Team,Stakeholder,Predictable_Velocity,Time to_Market,Value_Delivered,Quality,Response_to Change,Product_Owner,Team,Stakeholder,Predictable_Velocity,Time to_Market,Value_Delivered,Quality,Response_to Change,Product_Owner,Team,Stakeholder,Predictable_Velocity,Time to_Market,Value_Delivered,Quality,Response_to Change,Product_Owner,Team,Stakeholder,Predictable_Velocity,Time to_Market,Value_Delivered,Quality,Response_to Change
-6,2,5,6,9,6,3,2,2,8,9,8,3,2,5,10,2,10,9,10,7,3,2,9,2,4,6,3,2,6,2,8,5,8,9,3,2,7,5,7
-9,4,10,3,3,2,6,6,5,2,2,6,3,3,5,3,6,3,6,7,7,6,7,10,9,9,6,5,7,10,10,3,3,9,2,7,6,8,3,9
-4,4,3,10,3,10,10,2,9,10,5,8,7,9,8,5,10,8,6,3,7,2,10,7,2,5,2,6,4,2,8,10,6,6,6,5,10,5,3,7
-10,9,7,6,9,4,6,2,4,3,7,6,5,2,7,8,4,10,3,6,3,7,7,9,6,8,6,3,7,9,7,4,7,10,8,5,4,3,10,10
-4,9,10,4,9,6,6,8,10,4,7,8,6,10,4,10,8,3,10,8,5,4,7,10,8,7,6,5,4,7,7,4,5,5,3,3,2,6,6,6
-10,3,3,8,6,5,9,10,9,8,8,9,10,3,10,2,4,4,9,4,10,2,7,3,6,2,4,2,10,8,4,10,10,7,6,4,3,8,9,10
-3,6,2,8,5,9,7,10,6,8,5,3,5,2,4,6,5,10,2,2,9,4,6,4,5,9,2,8,3,4,10,3,7,9,5,10,4,7,9,9
-8,7,5,8,10,9,2,2,7,6,3,8,2,7,5,3,7,8,7,8,2,9,2,3,9,2,7,7,10,5,8,9,2,9,7,6,4,5,4,4
-5,6,5,4,10,3,8,2,6,7,7,4,6,9,5,7,9,6,9,2,3,2,8,7,2,10,5,8,10,2,8,9,2,8,5,2,10,2,8,9
-7,4,5,8,5,7,7,3,5,8,7,6,3,4,7,8,4,7,10,4,4,10,4,2,10,10,4,3,2,10,8,7,3,2,10,3,2,2,6,6
-9,6,8,7,8,2,5,7,9,3,5,3,9,2,8,9,5,10,2,6,5,10,6,6,7,3,2,4,4,5,4,9,3,3,5,3,6,10,3,5
-9,2,9,9,4,7,9,8,2,9,2,10,10,3,10,9,8,2,4,9,7,9,4,8,2,4,10,2,6,6,3,8,5,4,5,5,4,10,8,2
-5,6,7,9,6,6,10,3,10,9,3,9,9,9,4,2,5,7,3,9,6,6,5,8,4,8,2,9,7,6,5,9,8,6,2,3,9,10,5,9
-7,8,6,2,8,7,7,7,5,10,6,5,4,10,5,7,3,7,2,5,10,8,3,10,8,8,5,8,2,2,2,2,6,9,3,10,7,4,9,9
-8,3,7,2,6,2,2,8,3,9,7,6,3,6,7,10,4,5,7,3,3,2,6,2,2,10,7,6,8,5,9,2,2,4,8,2,9,4,10,5
-10,4,5,10,6,4,9,3,2,10,7,3,3,9,5,5,3,5,6,10,5,7,3,3,6,7,3,9,10,8,5,2,10,2,10,2,4,2,9,9
-5,8,9,6,2,6,2,5,7,4,7,5,7,7,9,7,10,4,4,9,7,4,9,9,7,7,2,4,10,4,9,9,2,9,10,10,2,9,6,4
-8,2,10,3,2,4,8,4,7,4,10,4,6,8,2,2,9,3,5,2,4,8,2,9,4,6,5,6,7,2,4,10,4,2,8,5,4,9,2,7
-6,5,9,2,7,8,3,7,8,8,2,5,6,8,6,9,9,8,3,10,9,9,5,5,9,9,8,9,10,6,8,6,10,9,6,6,4,5,4,9
-9,8,2,7,6,6,3,10,9,5,3,5,9,10,2,3,4,9,6,8,2,7,8,9,2,8,8,7,6,2,9,9,4,9,4,6,6,4,6,5`;
+`Happiness,Collaboration,Trust &_Respect,Creativity &_Innovation,Accountability,Vision &_Purpose,Goals &_Outcomes,Quarterly/_Roadmap,Monthly/_Release,Sprint,Roles &_Expectations,Sustainable_Pace,Self_Organization,Technical_Practices,Testing,Development_Practices,Release and_Deployment,Planning &_Estimating,Planning,Daily_Stand-Up,Review and_Demo,Retrospective,Size &_Skills,Effective_Facilitation,Leadership,Impediment_Management,Leadership,Technical_Expertise,Engagement,Engagement,Backlog_Management,Leadership,Leadership,Develop_People,Process_Improvement,Predictable_Delivery,Mean Time_To Recover,Change Failure_Rate,Test_Automation,Defect_Ratio,Story Cycle_Time,Impediments,Deployment_Frequency,Lead Time to_Deploy/Change,Business_Outcomes,Product Owner_Confidence,Team_Confidence
+7,8,8,6,8,8,6,6,6,7,9,6,7,5,2,4,7,4,7,8,6,6,6,2,5,5,7,7,0,6,6,8,8,0,0,9,0,8,1,9,1,7,3,0,9,0,9
+6,7,8,6,6,5,3,5,4,6,8,7,7,4,2,2,4,5,7,6,4,4,6,8,7,7,9,7,5,8,7,8,7,8,8,9,0,0,2,0,2,8,6,3,0,8,0
+7,8,8,7,9,7,7,8,9,8,8,7,7,7,2,5,4,8,8,6,7,5,6,8,7,7,9,9,9,8,9,6,0,0,0,9,8,8,0,0,0,9,9,4,0,0,6
+7,9,9,7,7,9,9,8,8,7,7,8,7,10,2,9,8,9,10,7,8,9,10,6,6,8,8,8,5,7,7,7,3,3,3,9,0,0,1,9,0,6,0,3,0,0,7
+8,7,4,8,9,4,4,7,3,8,7,8,9,4,2,5,9,5,8,7,7,6,7,7,5,5,0,0,0,4,7,6,7,8,5,9,0,8,0,7,3,7,4,4,1,0,9
+7,6,6,3,8,7,7,8,7,7,7,6,7,0,2,0,0,7,8,8,8,8,7,5,8,7,6,8,8,7,9,6,10,8,8,10,0,0,1,0,1,9,6,2,0,0,10`;
 
-const GROUPS = [[3, 'Team Facilitator'], [2, 'Team Lead(s)'], [3, 'Product Owner'], [3, 'Management'], [5, 'Team Dynamics'], [3, 'Team Facilitator'], [2, 'Team Lead(s)'], [3, 'Product Owner'], [3, 'Management'], [5, 'Team Dynamics'], [3, 'Doan'], [5, 'Sinh Man']];
-const CLASSES = [[4, 'LEADERSHIP', '#614d9c'], [1, 'CULTURE', '#da3b31'], [2, 'FOUNDATION', '#3953a0'], [3, 'CLARITY', '#efaa54'], [2, 'PERFORMANCE', '#4f8c4c']];
+const GROUPS = [[1, 'Happiness'], [1, 'Collaboration'], [1, 'Trust & Respect'], [1, 'Creativity & Innovation'], [1, 'Accountability'], [2, 'Vision'], [3, 'Planning'], [1, 'Roles'], [1, 'Sustainable Pace'], [1, ' Self Organization'], [4, 'Technical Excellence'], [1, 'Planning & Estimating'], [4, 'Effective Meeting'], [1, 'Size & Skills'], [3, 'Team Facilitator'], [3, 'Technical Owner'], [3, 'Product Owner'], [3, 'Management'], [1, 'Predictable Delivery'], [4, 'Quality'], [2, 'Responsiveness'], [2, 'Time to market'], [1, 'Value Delivery'], [2, 'Confidence']];
+const CLASSES = [[5, 'CULTURE', '#614d9c'], [3, 'CLARITY', '#da3b31'], [6, 'FOUNDATION', '#3953a0'], [4, 'LEADERSHIP', '#efaa54'], [6, 'PERFORMANCE', '#4f8c4c']];
